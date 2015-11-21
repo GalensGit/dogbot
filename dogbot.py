56c77e15b988605427c6e5317d43f354b6159ba4
galensgit <chrish.cataro@gmail.com>
2015-11-21 12:01:37 -0500

    added more pet dialogue

--- dogbot.py
+++ dogbot.py
@@ -35,6 +35,151 @@ def pet(message):
             message.chat.id,
             "You barely lifted your hand and Lesser Dog got excited."
         )
+    elif dog.pets == 2:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You lightly touched the Dog. It's already overexcited..."
+        )
+    elif dog.pets == 3:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You pet the Dog. It raises its head up to meet your hand."
+        )
+    elif dog.pets == 4:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You pet the Dog. It was a good Dog."
+        )
+    elif dog.pets == 5:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Lesser Dog is overstimulated."
+        )
+    elif dog.pets == 6:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You pet the Dog. Its excitement knows no bounds."
+        )
+    elif dog.pets == 7:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Critical pet! Dog excitement increased."
+        )
+    elif dog.pets == 8:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You have to jump up to pet the Dog."
+        )
+    elif dog.pets == 9:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You don't even pet it. It gets more excited."
+        )
+    elif dog.pets == 10:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Lesser Dog shows no signs of stopping."
+        )
+    elif dog.pets == 11:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "There is no way to stop this madness."
+        )
+    elif dog.pets == 12:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Lesser Dog enters the realm of the clouds."
+        )
+    elif dog.pets == 13:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "You call the Dog but it is too late. It cannot hear you."
+        )
+    elif dog.pets == 14:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "It's possible that you may have a problem."
+        )
+    elif dog.pets == 15:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Lesser Dog is learning to read."
+        )
+    elif dog.pets == 16:
+        dog.send_photo(
+            message.chat.id,
+            open('images/'+str(dog.pets)+'.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Lesser Dog is questioning your choices."
+        )
+    else:
+        dog.send_photo(
+            message.chat.id,
+            open('images/17.png', 'rb')
+        )
+        dog.send_message(
+            message.chat.id,
+            "Really..."
+        )
+    
 
 
 @dog.message_handler(commands=['version'])
